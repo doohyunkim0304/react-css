5b2c7c0889d2ee060fb31f85297aff77a44e1dc9
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-01-09 22:11:13 +0900

    Theming

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import styled, { createGlobalStyle, css} from "styled-components"
+import styled, { createGlobalStyle, ThemeProvider} from "styled-components"
 import "./App.css"
+import theme from './theme';
 
 createGlobalStyle`
   body{
@@ -9,35 +10,37 @@ createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
+const Card = styled.div`
+  background-color:white;
 `
 
-const Input = styled.input.attrs({
-  required:true
-})`
-  border-radious : 5px;
-  ${awesomeCard}
-`;
+const Button = styled.button`
+  border-radius:30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor}
+`
 
 class App extends Component {
   render() {
     return (
-    <Container>
-        <Input placeholder="hello" />
-    </Container>
+      <ThemeProvider theme={theme}>    
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     )
   }
 }
+
+
+const Form = () => (<Card>
+  <Button>Hello</Button>
+</Card>)
 
 const Container = styled.div`
   height:100vh;
   width:100%;
   background-color:#bdc3c7;
-  ${awesomeCard}
 
 `
 
